8e986ff6fc2e70000f0a9194e5d77de2ba6aa55b
machuangyu <zhongshankaka@163.com>
2020-07-28 13:50:59 +0800

    week05

---
+++ week05/toy-browser/server.js
@@ -0,0 +1,29 @@
+const http = require('http');
+
+http.createServer((request, response) => {
+  const { headers, method, url } = request;
+  let body = [];
+  request
+    .on('error', (err) => {
+      console.error(err);
+    })
+    .on('data', (chunk) => {
+      body.push(chunk);
+    })
+    .on('end', () => {
+      body = Buffer.concat(body).toString();
+      response.on('error', (err) => {
+        console.error(err);
+      });
+
+      response.statusCode = 200;
+      response.setHeader('Content-Type', 'application/json');
+
+      const responseBody = { headers, method, url, body };
+
+      response.write(`<div class="hello-world">hello world</div>`);
+      response.end();
+  });
+}).listen(8080);
+
+console.log('server started');
