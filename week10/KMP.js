8b679bff18e99d391c46594de3fe2bf8d694f86c
machuangyu <zhongshankaka@163.com>
2020-09-06 17:21:47 +0800

    week10

---
+++ week10/KMP.js
@@ -0,0 +1,39 @@
+function KMP(source, pattern) {
+  // 计算table
+  let table = new Array(pattern.length).fill(0);
+
+  {
+    let i = 1,
+      j = 0;
+    while (i < pattern.length) {
+      if (pattern[i] === pattern[j]) {
+        i++, j++;
+        table[i] = j;
+      } else {
+        if (j > 0) {
+          j = table[j]
+        } else {
+          ++i
+        }
+      }
+    }
+  } {
+    let i = 0,
+      j = 0;
+    while (i < source.length) {
+
+      if (pattern[j] === source[i]) {
+        ++i, ++j;
+      } else {
+        if (j > 0) {
+          j = table[j]
+        } else {
+          ++i
+        }
+      }
+      console.log(i, j);
+      if (j === pattern.length) return true;
+    }
+    return false;
+  }
+}
